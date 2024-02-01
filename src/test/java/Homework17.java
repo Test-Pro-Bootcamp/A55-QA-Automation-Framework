460ec320b11af211eb52af5e89119c653fd23e71
Aika889 <aida.taymaskhanova@testpro.io>
2024-01-31 19:47:40 -0500

    Unfinished commit need to add assertion

--- src/test/java/Homework17.java
+++ src/test/java/Homework17.java
@@ -1,13 +1,13 @@
 //import org.openqa.selenium.chrome.ChromeDriver;//
 //import org.openqa.selenium.chrome.ChromeOptions;//
-import org.openqa.selenium.interactions.Action;
-import org.openqa.selenium.interactions.Actions;
+//import org.openqa.selenium.interactions.Action;//
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.openqa.selenium.By;
 //import org.openqa.selenium.WebDriver;//
 import org.openqa.selenium.WebElement;
 
-import java.time.Duration;
+//import java.time.Duration;//
 
 public class Homework17 extends BaseTest {
 
@@ -24,10 +24,7 @@ public class Homework17 extends BaseTest {
         clickViewAll();
         ClickSong();
         ClickAddTo();
-
-
-
-
+        //SuccessPopOutMessage();
 
     }
 
@@ -51,22 +48,36 @@ public class Homework17 extends BaseTest {
     @Test
     public void ClickSong() throws InterruptedException {
         WebElement FirstSong = driver.findElement(By.xpath("//td[contains(text(),'Dark Days')]"));
-        Actions action = new Actions(driver);
-        action.contextClick(FirstSong).perform();
+         FirstSong.click();
+        //Actions action = new Actions(driver);
+        //action.contextClick(FirstSong).perform();
         Thread.sleep(2000);
     }
 
     @Test
     public void ClickAddTo(){
-        WebElement AddTo = driver.findElement(By.cssSelector("li[class='has-sub']"));
+        //WebElement AddTo = driver.findElement(By.cssSelector("li[class='has-sub']"));
+        WebElement AddTo = driver.findElement(By.cssSelector("button[class='btn-add-to']"));
+        AddTo.click();
     }
 
     @Test
-    public void ClickPlaylistName() throws InterruptedException {
-        WebElement PlayListName = driver.findElement(By.cssSelector("a[href='#!/playlist/86987']"));
+    public void ChoosePlaylistName() throws InterruptedException {
+        WebElement PlayListName = driver.findElement(By.cssSelector("//section[@id='songResultsWrapper']//li[@class='playlist'][normalize-space()='test1']"));
         PlayListName.click();
         Thread.sleep(2000);
     }
+
+    //@Test
+    //public boolean SuccessPopOutMessage(){
+
+        //WebElement PopOut = driver.findElement(By.cssSelector("//div[@class='success show']"));
+        //String expectedText = "Added 1 song to test1";
+        //String actualText = driver.getText();
+        //Assert.assertEquals(expectedText, actualText);
+        //return PopOut.isDisplayed();
+
+    //}
 
 
 }
