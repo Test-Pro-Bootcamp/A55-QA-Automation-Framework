03c50f17091b2593d96466775bbf3dd964c9432c
Aika889 <aida.taymaskhanova@testpro.io>
2024-02-02 15:13:52 -0500

    Homework18.java

---
+++ src/test/java/Homework18.java
@@ -0,0 +1,16 @@
+import org.testng.annotations.Test;
+
+public class Homework18 extends BaseTest {
+    @Test
+    public void playSong (){
+        navigateToUrl();
+        provideEmail("aida.taymaskhanova@testpro.io");
+        providePassword("Ozzikpozzik18");
+        clickSubmit();
+
+    }
+    @Test
+    public void
+
+
+}
