f34974c2545d48490afcdebc6baa6333a7c4118c
Mohamed Taqi <126124586+MedTaqi@users.noreply.github.com>
2024-02-03 07:44:59 -0500

    Homework19

--- src/test/java/Homework19.java
+++ src/test/java/Homework19.java
@@ -1,15 +1,33 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Homework19 extends BaseTest {
-    String DeleteMessage = "Deleted playlist vivo.";
+    String DeleteMessage = "Deleted playlist \"vivo.\"";
+    String createdPlaylist = "Created playlist \"vivo.\"";
+
+    @Test
+    public void createPlaylist() throws InterruptedException {
+        //Given
+        provideEmail("taqimed99@gmail.com");
+        providePassword("Med-20115-010499@");
+        clickBtn();
+        //When
+        clickPlusButton();
+        chooseNewPlaylist();
+        enterPlaylistName("vivo");
+        //Then
+        Thread.sleep(5000);
+        Assert.assertEquals(playlistCreated(), createdPlaylist);
+
+    }
 
     @Test
 
     public void deletePlaylist() throws InterruptedException {
-        String DeleteMessage = "Deleted playlist \"vivo.\"";
         //Given
         provideEmail("taqimed99@gmail.com");
         providePassword("Med-20115-010499@");
@@ -24,12 +42,34 @@ public class Homework19 extends BaseTest {
         Assert.assertEquals(DeleteMsg(), DeleteMessage);
     }
 
+    public String playlistCreated() {
+        WebElement notificationMsg = driver.findElement(By.cssSelector("div.success.show"));
+        return notificationMsg.getText();
+    }
+
+    private void enterPlaylistName(String newPlaylistName) {
+        WebElement playlistNameField = driver.findElement(By.cssSelector("[name='name'] "));
+        playlistNameField.clear();
+        playlistNameField.sendKeys(newPlaylistName);
+        playlistNameField.sendKeys(Keys.ENTER);
+    }
+
+    private void chooseNewPlaylist() {
+        WebElement newPlaylist = driver.findElement(By.cssSelector("[data-testid='playlist-context-menu-create-simple']"));
+        newPlaylist.click();
+    }
+
+    private void clickPlusButton() {
+        WebElement clickPlusButton1 = driver.findElement(By.cssSelector("[data-testid='sidebar-create-playlist-btn']"));
+        clickPlusButton1.click();
+    }
+
     public String DeleteMsg() {
         WebElement msg = driver.findElement(By.cssSelector("div.success.show"));
         return msg.getText();
     }
 
-    private void clickDeleteButton() {
+    public void clickDeleteButton() {
         WebElement DeleteBtn = driver.findElement(By.cssSelector("[class='del btn-delete-playlist']"));
         DeleteBtn.click();
     }
