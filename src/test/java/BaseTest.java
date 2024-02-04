49b5dc8166dc0b603364d576048ff64a7177b770
nayanarao <nayana.rao.s@gmail.com>
2024-02-04 09:51:27 +0530

    set up program

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,5 +1,7 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
 public class BaseTest {
@@ -7,5 +9,10 @@ public class BaseTest {
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeMethod
+    public void launchBrowser(){
+        WebDriver 
     }
 }
---
+++ src/test/java/homework20.java
@@ -0,0 +1,33 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+
+public class homework20 extends BaseTest{
+    public void deletePlaylist() throws InterruptedException {
+        enterEmail();
+        enterPassword();
+        loginButton();
+        ChoosePlaylist();
+        deleteThePlaylist();
+        confirmDeletion();
+    }
+
+    private void confirmDeletion() {
+        String successMessage = "Deleted playlist \"apple.\"";
+        WebElement messageSuccess = driver.findElement(By.xpath("//div[@class=\"alertify-logs top right\"]/div[@class=\"success show\"]"));
+        Assert.assertEquals(messageSuccess.getText(),successMessage);
+    }
+
+    private void deleteThePlaylist() throws InterruptedException {
+        WebElement deleteButton = driver.findElement(By.cssSelector("[title=\"Delete this playlist\"]"));
+        deleteButton.click();
+        WebElement okayButton = driver.findElement(By.xpath("//div[@class=\"alertify\"]//nav/button[@class=\"ok\"]"));
+        okayButton.click();
+        Thread.sleep(900);
+    }
+
+    private void ChoosePlaylist() {
+        WebElement chosenPlaylist = driver.findElement(By.xpath("//*[@id=\"playlists\"]//a[text() = \"apple\"]"));
+        chosenPlaylist.click();
+    }
+}
