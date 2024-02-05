a54ad441d443f0403e45241d7b4ba18fb737382e
ananicora <157588200+ananicora@users.noreply.github.com>
2024-02-06 00:13:11 +0100

    Homework20

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -2,6 +2,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
--- src/test/java/DeletePlayList.java
+++ src/test/java/DeletePlayList.java
@@ -4,109 +4,114 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 import java.time.Duration;
+import java.time.Instant;
 
-public class DeletePlayList extends  BaseTest{
-  //  Navigate to "https://qa.koel.app/".
-  // Log in with your credentials.
+public class DeletePlayList extends BaseTest {
+    //  Navigate to "https://qa.koel.app/".
+    // Log in with your credentials.
 
-  @BeforeMethod
-  @Parameters({"BaseURL", "Email", "Password"})
-  public void start(String baseURL, String email, String password) {
-    ChromeOptions options = new ChromeOptions();
-    options.addArguments("--remote-allow-origins=*");
+    @BeforeMethod
+    @Parameters({"BaseURL", "Email", "Password"})
+    public void start(String baseURL, String email, String password) {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
 
-    driver = new ChromeDriver(options);
-    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
-
-    loginValidEmailPassword(baseURL, email, password);
-  }
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
 
-  private void loginValidEmailPassword(String baseURL, String email, String password) {
-    //Steps 1: Open Browser and navigate to Koel app.
-    navigateToPage(baseURL);
-    //Step 2: Enter email
-    provideEmail(email);
-    //Step 3: Enter Password
-    providePassword(password);
-    //Step 4: Click on Login button
-    loginToKoel();
-    //Assertion (expected vs actual)
-    WebElement avatarIcon = driver.findElement(By.cssSelector("img[class='avatar']"));
-    Assert.assertTrue(avatarIcon.isDisplayed());
-  }
+        loginValidEmailPassword(baseURL, email, password);
+    }
 
-  @Test
-  public void deletePlayList() throws InterruptedException {
-      String playlist = "my musik";
 
-      clickPlusButton();
-      chooseNewPlaylist();
-      enterPlaylistName(playlist);
-      Thread.sleep(500);
+    private void loginValidEmailPassword(String baseURL, String email, String password) {
+        //Steps 1: Open Browser and navigate to Koel app.
+        navigateToPage(baseURL);
+        //Step 2: Enter email
+        provideEmail(email);
+        //Step 3: Enter Password
+        providePassword(password);
+        //Step 4: Click on Login button
+        loginToKoel();
+        //Assertion (expected vs actual)
+        WebElement avatarIcon = driver.findElement(By.cssSelector("img[class='avatar']"));
+        Assert.assertTrue(avatarIcon.isDisplayed());
+    }
 
-      String createdMsg = "Created playlist \""+playlist+".\"";
-      Assert.assertEquals(playlistCreated(), createdMsg);
-      Thread.sleep(5000);
+    @Test
+    @Parameters({"PlayList"})
+    public void deletePlayList(String playList) throws InterruptedException {
+        clickPlusButton();
+        chooseNewPlaylist();
+        enterPlaylistName(playList);
 
-      //Click the playlist you want to delete.
-      clickPlaylist();
+        String createdMsg = "Created playlist \"" + playList + ".\"";
+        Assert.assertEquals(getNotification(), createdMsg);
 
-      // We should see a red button "x PLAYLIST" on the top right part of the page, and click on it.
-      deleteButton();
-      Thread.sleep(500);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector("div.success.show")));
 
-      //  Verify that the confirmation notification displayed has the text, "Deleted playlist {playlist name}".
-      String message = "Deleted playlist \""+playlist+".\"";
-      Assert.assertEquals(getDeleteMessage(), message);
-  }
 
-  public void clickPlaylist() {
-    WebElement playlistBtn =  driver.findElement(By.xpath("//section[@id='playlists']//a[text()='my musik']"));
-    playlistBtn.click();
+        //Click the playlist you want to delete.
+        clickPlaylist(playList);
 
-  }
- public void deleteButton() {
-   WebElement deleteBtn =  driver.findElement(By.xpath("//button[@class='del btn-delete-playlist']"));
-    deleteBtn.click();
- }
+        // We should see a red button "x PLAYLIST" on the top right part of the page, and click on it.
+        deleteButton();
 
- public String getDeleteMessage() {
-   WebElement msg = driver.findElement(By.cssSelector("div.success.show"));
-   return msg.getText();
- }
+        //  Verify that the confirmation notification displayed has the text, "Deleted playlist {playlist name}".
+        String message = "Deleted playlist \"" + playList + ".\"";
+        Assert.assertEquals(getNotification(), message);
+    }
 
-  public String playlistCreated() {
-    WebElement notificationMsg = driver.findElement(By.cssSelector("div.success.show"));
-    return notificationMsg.getText();
-  }
+    public void clickPlaylist(String playlist) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        WebElement playlistBtn = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//section[@id='playlists']//a[text()='"+ playlist +"']")));
+        playlistBtn.click();
+    }
+
+    public void deleteButton() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        WebElement deleteBtn = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@class='del btn-delete-playlist']")));
+        deleteBtn.click();
+    }
+
+
+    public String getNotification() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        WebElement notificationMsg = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.success.show")));
+
+        return notificationMsg.getText();
+    }
 
     public void enterPlaylistName(String newPlaylistName) {
-    WebElement playlistNameField = driver.findElement(By.cssSelector("input[placeholder='↵ to save']"));
-    playlistNameField.clear();
-    playlistNameField.sendKeys(newPlaylistName);
-    playlistNameField.sendKeys(Keys.ENTER);
-  }
+        WebElement playlistNameField = driver.findElement(By.cssSelector("input[placeholder='↵ to save']"));
+        playlistNameField.clear();
+        playlistNameField.sendKeys(newPlaylistName);
+        playlistNameField.sendKeys(Keys.ENTER);
+    }
 
     public void chooseNewPlaylist() {
-    WebElement newPlaylist = driver.findElement(By.cssSelector("#playlists > nav > ul > li:nth-child(1)"));
-    newPlaylist.click();
-  }
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        WebElement newPlaylist = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#playlists > nav > ul > li:nth-child(1)")));
+        newPlaylist.click();
+    }
 
-  public void clickPlusButton() throws InterruptedException {
-    WebElement btn = driver.findElement(By.cssSelector("#playlists > h1 > i"));
-    Actions s = new Actions(driver);
-    s.moveToElement(btn).build().perform();
-    Thread.sleep(500);
-    s.click(btn).build().perform();
-  }
+    public void clickPlusButton() throws InterruptedException {
+        WebElement btn = driver.findElement(By.cssSelector("#playlists > h1 > i"));
+        Actions s = new Actions(driver);
+        s.moveToElement(btn).build().perform();
+        s.pause(100).build().perform();
+        s.click(btn).build().perform();
+    }
 
 
 }
