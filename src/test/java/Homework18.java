2339b12dc5b8f923907276546819f4a165e2edd8
Aika889 <aida.taymaskhanova@testpro.io>
2024-02-05 14:20:07 -0500

    innitial commit

--- src/test/java/Homework18.java
+++ src/test/java/Homework18.java
@@ -1,4 +1,9 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import javax.sound.midi.Soundbank;
 
 public class Homework18 extends BaseTest {
     @Test
@@ -7,10 +12,27 @@ public class Homework18 extends BaseTest {
         provideEmail("aida.taymaskhanova@testpro.io");
         providePassword("Ozzikpozzik18");
         clickSubmit();
+        clickPlayNextSong();
+        clickPlay();
+        Assert.assertTrue(SoundBarIsDisplayed());
+    }
+
+    public boolean SoundBarIsDisplayed() {
+        WebElement SoundBar = driver.findElement(By.cssSelector("img[alt='Sound bars']"));
+        return(SoundBar.isDisplayed());
 
     }
-    @Test
-    public void
+
+    public void clickPlay() {
+        WebElement PlaySong = driver.findElement(By.cssSelector("i[class='fa fa-play']"));
+        PlaySong.click();
+    }
+
+    public void clickPlayNextSong() {
+        WebElement NextSongBtn = driver. findElement(By.cssSelector("i[title='Play next song']"));
+        NextSongBtn.click();
+    }
+
 
 
 }
