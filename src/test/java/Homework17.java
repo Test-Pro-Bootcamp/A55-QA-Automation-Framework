d0a9adb60f4c45d255b1663f970db0456979df35
Aika889 <aida.taymaskhanova@testpro.io>
2024-02-07 13:46:38 -0500

    innitial homework19 commit. Added parameters in TestNG.xml file. In BaseTest.java added @patrameters annotations with BaseURL(same as in .xml file.Removed hard coded koel.app link. InBaseTest added method navigateToURL(baseURL);

--- src/test/java/Homework17.java
+++ src/test/java/Homework17.java
@@ -8,7 +8,7 @@ public class Homework17 extends BaseTest {
     @Test
     public void addSongToPlaylist() throws InterruptedException {
         //navigate to page
-        navigateToUrl();
+        //navigateToUrl();
         //login with to koel.app
         provideEmail("aida.taymaskhanova@testpro.io");
         providePassword("Ozzikpozzik18");
@@ -19,8 +19,6 @@ public class Homework17 extends BaseTest {
         clickAddTo();
         Assert.assertTrue(successPopOutMessage());
     }
-
-    @Test
     public void enterSongToSearchBar() throws InterruptedException {
             WebElement SearchField = driver.findElement(By.cssSelector("input[type='search']"));
             SearchField.clear();
@@ -29,13 +27,12 @@ public class Homework17 extends BaseTest {
             SearchField.click();
             Thread.sleep(2000);
     }
-    @Test
     public void clickViewAll() throws InterruptedException {
         WebElement ViewAllBtn = driver.findElement(By.cssSelector("button[data-test='view-all-songs-btn']"));
         ViewAllBtn.click();
         Thread.sleep(2000);
     }
-    @Test
+
     public void clickSong() throws InterruptedException {
         WebElement FirstSong = driver.findElement(By.xpath("//td[contains(text(),'Dark Days')]"));
          FirstSong.click();
@@ -43,19 +40,17 @@ public class Homework17 extends BaseTest {
         //action.contextClick(FirstSong).perform();
         Thread.sleep(2000);
     }
-    @Test
+
     public void clickAddTo(){
         //WebElement AddTo = driver.findElement(By.cssSelector("li[class='has-sub']"));
         WebElement AddTo = driver.findElement(By.cssSelector("button[class='btn-add-to']"));
         AddTo.click();
     }
-    @Test
     public void choosePlaylistName() throws InterruptedException {
         WebElement PlayListName = driver.findElement(By.cssSelector("//section[@id='songResultsWrapper']//li[@class='playlist'][normalize-space()='test1']"));
         PlayListName.click();
         Thread.sleep(2000);
     }
-    @Test
     public boolean successPopOutMessage(){
         WebElement PopOut = driver.findElement(By.cssSelector("//div[@class='success show']"));
         Assert.assertTrue(PopOut.isDisplayed());
