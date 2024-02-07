58f3a09c345b57b18db16fbd1b8c220aeaa5e33e
Mohamed Taqi <126124586+MedTaqi@users.noreply.github.com>
2024-02-07 10:47:41 -0500

    Training_Test

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,11 +1,72 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Parameters;
+
+import java.time.Duration;
+
 
 public class BaseTest {
-
+    WebDriver driver;
+    WebDriverWait wait;
+    String url ;
+    Actions actions;
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeMethod
+    @Parameters({"BaseUrl"})
+    public void launchBrowser(String BaseUrl){
+        ChromeOptions  ops = new ChromeOptions();
+        ops.addArguments("--remote-allow-origins=*");
+        ops.addArguments("--disable-notifications");
+        driver = new ChromeDriver(ops);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        driver.manage().window().maximize();
+        wait = new WebDriverWait(driver,Duration.ofSeconds(5));
+        actions = new Actions(driver);
+        url = BaseUrl;
+        driver.get(url);
+        Assert.assertEquals(driver.getCurrentUrl(), url);
+    }
+
+    public void provideEmail(String email) {
+        WebElement emailField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[type='email']")));
+        emailField.clear();
+        emailField.sendKeys(email);
+    }
+
+    public void providePassword(String password) {
+        WebElement passwordField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[type='password']")));
+        passwordField.clear();
+        passwordField.sendKeys(password);
+    }
+
+    public void clickBtn() {
+        //WebElement submitBtn = driver.findElement(By.cssSelector("button[type='submit']"));
+        WebElement submitBtn = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[type='submit']")));
+        submitBtn.click();
+    }
+    public void login(){
+        provideEmail("taqimed99@gmail.com");
+        providePassword("Med-20115-010499@");
+        clickBtn();
+    }
+
+    @AfterMethod
+    public void closeBrowser() {
+        driver.quit();
     }
 }
