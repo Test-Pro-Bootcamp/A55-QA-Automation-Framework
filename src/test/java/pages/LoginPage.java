baca58bba0900614a7f052f9f47f0dd0d3f7c707
Mohamed Taqi <126124586+MedTaqi@users.noreply.github.com>
2024-02-08 07:31:40 -0500

    Homework22

---
+++ src/test/java/pages/LoginPage.java
@@ -0,0 +1,36 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class LoginPage extends BasePage {
+
+    By emailField = By.cssSelector("[type='email']");
+    By passwordField = By.cssSelector("[type='password']");
+    By clickBtn1 = By.cssSelector("[type='submit']");
+
+
+    public LoginPage(WebDriver givenDriver) {
+        super(givenDriver);
+    }
+
+    public void provideEmail(String email) {
+        findElement(emailField).sendKeys(email);
+    }
+
+    public void providePassword(String password) {
+        findElement(passwordField).sendKeys(password);
+    }
+
+    public void clickBtn() {
+        findElement(clickBtn1).click();
+    }
+
+    public void logIn() {
+        provideEmail("taqimed99@gmail.com");
+        providePassword("Med-20115-010499@");
+        clickBtn();
+    }
+}
