42170d18b3412852573f2f618934d0447249c4f6
Aika889 <aida.taymaskhanova@testpro.io>
2024-02-13 19:43:07 -0500

    Corrected Homework 21

--- src/test/java/Homework21.java
+++ src/test/java/Homework21.java
@@ -7,24 +7,25 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Homework21 extends BaseTest {
-
+    String NewPlaylistName = "new_list";
     @Test
-    public void renamePlayList() throws InterruptedException {
+    public void renamePlayList()   {
         String expectedSuccessUpdateMessage = "Updated playlist \"my_list..\"";
         //navigateToUrl();
         provideEmail("aida.taymaskhanova@testpro.io");
         providePassword("Ozzikpozzik18");
         clickSubmit();
-        rightClickPlaylist();
-        clickEdit();
-        //clearTheField();
+        doubleClickPlaylist();
         enterNewName();
+        //clearTheField();
         Assert.assertEquals(getSuccessUpdatedPlayListMessage(), expectedSuccessUpdateMessage );
     }
 
     public void enterNewName() {
-        WebElement NewName = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("a[class='active']")));
-        //NewName.clear();
+        WebElement NewName = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[name='name']")));
+        //clear does not work, element has an attribute of 'required'
+        //workaround is ctrl A(to select all) then backspace to clear then replace with
+        // with new playlist mame
         NewName.sendKeys(Keys.chord(Keys.COMMAND, "A", Keys.BACK_SPACE));
         NewName.sendKeys("new_list");
         NewName.sendKeys(Keys.ENTER);
@@ -34,19 +35,14 @@ public class Homework21 extends BaseTest {
     //WebElement InputField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[name='name']")));
     //InputField.clear();
     //}
-    public void clickEdit() {
-        WebElement EditBtn = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("li[data-testid='playlist-context-menu-edit-89290']")));
+
+    public void doubleClickPlaylist() {
+        WebElement PlayList = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("a[href='#!/playlist/90824']")));
         Actions action = new Actions(driver);
-        action.contextClick(EditBtn).perform();
+        action.doubleClick(PlayList).perform();
     }
-    public void rightClickPlaylist() {
-        WebElement PlayList = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("a[href='#!/playlist/89290']")));
-        Actions action = new Actions(driver);
-        action.contextClick(PlayList).perform();
-    }
-    public String getSuccessUpdatedPlayListMessage() throws InterruptedException {
-        WebElement messagePopUP = driver.findElement(By.xpath("//div[@class='success show']"));
-        Thread.sleep(2000);
+    public String getSuccessUpdatedPlayListMessage()  {
+        WebElement messagePopUP = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("div[class='success.show']")));
         return messagePopUP.getText();
     }
 }
