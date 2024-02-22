1ed0ca5ab4781bf7e9a28884b926d6854ec5cf52
Aika889 <aida.taymaskhanova@testpro.io>
2024-02-21 20:37:58 -0500

    Homework - 24 commit

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -8,12 +8,17 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Parameters;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.net.MalformedURLException;
+import java.net.URI;
 import java.time.Duration;
 public class BaseTest {
     public static WebDriver driver;//= null ;public static String url = null;
@@ -22,13 +27,12 @@ public class BaseTest {
     public String baseURL = "https://qa.koel.app/";
     @BeforeSuite
     static void setupClass() {
-        WebDriverManager.firefoxdriver().setup();
-        WebDriverManager.chromedriver().setup();
-        WebDriverManager.edgedriver().setup();
+        //WebDriverManager.firefoxdriver().setup();
+
     }
     @BeforeMethod
     @Parameters({"url"})
-    public void launchBrowser(String url) {
+    public void launchBrowser(String url) throws MalformedURLException {
         driver = pickBrowser(System.getProperty("browser"));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         //maximize window
@@ -38,7 +42,9 @@ public class BaseTest {
         navigateToURL(url);
         actions = new Actions(driver);
     }
-    public static WebDriver pickBrowser(String browser) {
+    public static WebDriver pickBrowser(String browser) throws MalformedURLException {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        String gridURL = "http://192.168.1.203:4444/";
         switch (browser) {
             case "firefox":
                 WebDriverManager.firefoxdriver().setup();
@@ -48,6 +54,16 @@ public class BaseTest {
                 EdgeOptions edgeOptions = new EdgeOptions();
                 edgeOptions.addArguments("--remote-allow-origins=*");
                 return driver = new EdgeDriver(edgeOptions);
+            case "grid-edge":
+                caps.setCapability("browserName", "MicrosoftEdge");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), caps);
+            case "grid=firefox" :
+                caps.setCapability("browserName", "firefox");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), caps);
+            case "grid=chrome" :
+                caps.setCapability("browserName", "chrome");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), caps);
+
             default:
                 WebDriverManager.chromedriver().setup();
                 ChromeOptions chromeOptions = new ChromeOptions();
