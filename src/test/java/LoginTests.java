27d7d7894a9b8001d252b5b5683b829e9daf609a
Aika889 <aida.taymaskhanova@testpro.io>
2024-02-22 23:00:47 -0500

    commit with changes in .xml file

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -7,75 +7,44 @@ import pages.LogInPage;
 public class LoginTests extends BaseTest {
 
     @Test
-    public void loginValidEmailPasswordTest() throws InterruptedException {
+    public void loginValidEmailPasswordTest() {
         //creating objects
         LogInPage loginPage = new LogInPage(getDriver());
         HomePage homePage = new HomePage(getDriver());
-        Thread.sleep(2000);
         LogInPage.provideEmail("aida.taymaskhanova@testpro.io");
-        Thread.sleep(2000);
         LogInPage.providePassword("Ozzikpozzik18");
-        Thread.sleep(2000);
         LogInPage.clickSubmit();
-        Thread.sleep(2000);
         Assert.assertTrue(homePage.getUserAvatar().isDisplayed());
-        Thread.sleep(2000);
     }
 
     @Test
-    public void loginInvalidEmailValidPasswordTest() throws InterruptedException {
+    public void loginInvalidEmailValidPasswordTest() {
         LogInPage loginPage = new LogInPage(getDriver());
         HomePage homePage = new HomePage(getDriver());
-        Thread.sleep(2000);
         LogInPage.provideEmail("taymaskhanova@testpro.io");
-        Thread.sleep(2000);
         LogInPage.providePassword("Ozzikpozzik18");
-        Thread.sleep(2000);
         LogInPage.clickSubmit();
         //Assert.assertTrue(homePage.getUserAvatar().isDisplayed());
     }
 
     @Test
-    public void loginEmptyEmailAndPasswordTest() throws InterruptedException {
+    public void loginEmptyEmailAndPasswordTest(){
         LogInPage loginPage = new LogInPage(getDriver());
         HomePage homePage = new HomePage(getDriver());
-        Thread.sleep(2000);
         LogInPage.provideEmail("");
-        Thread.sleep(2000);
         LogInPage.providePassword("");
-        Thread.sleep(2000);
         LogInPage.clickSubmit();
         //Assert.assertTrue(homePage.getUserAvatar().isDisplayed());
     }
 
     @Test
-    public void loginEmptyEmailValidPasswordTest() throws InterruptedException {
+    public void loginEmptyEmailValidPasswordTest() {
         LogInPage loginPage = new LogInPage(getDriver());
         HomePage homePage = new HomePage(getDriver());
-        Thread.sleep(2000);
         LogInPage.provideEmail("taymaskhanova@testpro.io");
-        Thread.sleep(2000);
         LogInPage.providePassword("Ozzikpozzik18");
-        Thread.sleep(2000);
         LogInPage.clickSubmit();
         //Assert.assertTrue(homePage.getUserAvatar().isDisplayed());
     }
-
-
-
-
-
-
-//      Added ChromeOptions argument below to fix websocket error
-        //ChromeOptions options = new ChromeOptions();
-        //options.addArguments("--remote-allow-origins=*");
-
-        //WebDriver driver = new ChromeDriver(options);
-        //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
-        //String url = "https://qa.koel.app/";
-        //driver.get(url);
-        //Assert.assertEquals(driver.getCurrentUrl(), url);
-        //driver.quit();
-    }
+}
 
