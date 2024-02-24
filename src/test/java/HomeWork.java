9dd36610bf08331e62fa25379454570a48eee5ba
nayanarao <nayana.rao.s@gmail.com>
2024-02-24 14:21:13 +0530

    update1

--- src/test/java/HomeWork.java
+++ src/test/java/HomeWork.java
@@ -16,33 +16,4 @@ public class HomeWork extends BaseTest{
         homeWorkPage.deletePlaylists();
         homeWorkPage.confirmDel();
     }
-
-    @Test
-    public void deletePlaylist() throws InterruptedException {
-        enterEmail();
-        enterPassword();
-        loginButton();
-        ChoosePlaylist();
-        deleteThePlaylist();
-        confirmDeletion();
-    }
-
-    private void confirmDeletion() {
-        String successMessage = "Deleted playlist \"apple.\"";
-        WebElement messageSuccess = driver.findElement(By.xpath("//div[@class=\"alertify-logs top right\"]/div[@class=\"success show\"]"));
-        Assert.assertEquals(messageSuccess.getText(),successMessage);
-    }
-
-    private void deleteThePlaylist() throws InterruptedException {
-        WebElement deleteButton = driver.findElement(By.cssSelector("[title=\"Delete this playlist\"]"));
-        deleteButton.click();
-        WebElement okayButton = driver.findElement(By.xpath("//div[@class=\"alertify\"]//nav/button[@class=\"ok\"]"));
-        okayButton.click();
-        Thread.sleep(900);
-    }
-
-    private void ChoosePlaylist() {
-        WebElement chosenPlaylist = driver.findElement(By.xpath("//*[@id=\"playlists\"]//a[text() = \"apple\"]"));
-        chosenPlaylist.click();
-    }
 }
