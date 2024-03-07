804cd26fad9ec5bac0162ac5cbf20493e2b35365
NayanaRaoSubramanya <157223846+NayanaRaoSubramanya@users.noreply.github.com>
2024-03-07 14:48:37 +0530

    changed code to reflect null as an input for pickbrowser()

--- src/test/java/step_definition/LoginStepDef.java
+++ src/test/java/step_definition/LoginStepDef.java
@@ -1,7 +1,6 @@
-package Step_Definiton;
+package step_definition;
 
 import io.cucumber.java.After;
-import io.cucumber.java.Before;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -12,7 +11,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
